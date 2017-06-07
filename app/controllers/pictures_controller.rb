e4b7aad5b57dfc7cd29f530db23dded8902db797
Blueibis <jennie.li.2011@gmail.com>
2017-06-07 16:16:39 -0400

    Implement new, create

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,4 +6,22 @@ class PicturesController < ApplicationController
   def show
     @picture = Picture.find(params[:id])
   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to "/pictures"
+    else
+      render :new
+    end
+  end
+
+  def picture_params
+   { title: params[:picture][:title], artist: params[:picture][:artist], url: params[:picture][:url] }
+  end
+
 end
